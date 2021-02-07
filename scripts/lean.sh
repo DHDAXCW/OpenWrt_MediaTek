5397171ee29dd4898feb6b75b454f13544ab4993
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-02-07 17:12:22 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -130,10 +130,6 @@ popd
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
-# wendu
-sed -i "/<%:Load Average%>/i\\\t\t<tr><td width="33%"><%:CPU Temperature%></td><td><%=luci.sys.exec(\"cut -c1-3 /sys/class/thermal/thermal_zone0/temp | awk '{print \$1/10.0}'\")%>℃</td></tr>" /usr/lib/lua/luci/view/admin_status/index.htm
-rm /lib/preinit/82_show_temp
-
 # Custom configs
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
