597cf54cf15b2d54e02a41b8e73dc9a2c1addd00
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-02-07 12:01:34 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -140,7 +140,6 @@ echo -e " Lean's OpenWrt built on "$(date +%Y.%m.%d)"\n ------------------------
 
 # wendu
 sed -i "/<%:Load Average%>/i\\\t\t<tr><td width="33%"><%:CPU Temperature%></td><td><%=luci.sys.exec(\"cut -c1-3 /sys/class/thermal/thermal_zone0/temp | awk '{print \$1/10.0}'\")%>℃</td></tr>" /usr/lib/lua/luci/view/admin_status/index.htm
-rm /lib/preinit/82_show_temp
 
 pushd package/lean
 # Add Project OpenWrt's autocore
