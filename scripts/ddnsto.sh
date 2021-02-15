d8456cb6626029dbd89174104a004035464d3f1c
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-02-15 23:18:59 +0800

    Update ddnsto.sh

--- scripts/ddnsto.sh
+++ scripts/ddnsto.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-opkg update
-#cd /tmp/upload
+#opkg update
+cd /tmp/upload
 wget https://firmware.koolshare.cn/binary/ddnsto/openwrt/ddnsto-arm.ipk
 wget https://firmware.koolshare.cn/binary/ddnsto/openwrt/luci-app-ddnsto_1.0.0-1_all.ipk
 wget https://firmware.koolshare.cn/binary/ddnsto/openwrt/luci-i18n-ddnsto-zh-cn_1.0.0-1_all.ipk
