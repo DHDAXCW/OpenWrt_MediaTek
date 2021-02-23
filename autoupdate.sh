2ada946de5e8e400e39f21e8dd06e1f5e613a69e
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-02-23 13:36:41 +0800

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,21 +1,22 @@
 #!/bin/sh --Created by DHDAXCW
 opkg update
-mkdir /mnt/mmcblk0p2
-cd /mnt/mmcblk0p2
-rm -rf /mnt/mmcblk0p2/openwrt-rockchip*.img.gz 
+cd /tmp/upload
+rm -rf /tmp/upload/openwrt-rockchip*.img.gz sha256sums
 wget https://github.com/DHDAXCW/NanoPi-R4S-2021/releases/download/$(date +%Y.%m.%d)-Lean/openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz
-
-if [ -f /mnt/mmcblk0p2/openwrt-rockchip*.img.gz	]; then
+wget https://github.com/DHDAXCW/NanoPi-R4S-2021/releases/download/$(date +%Y.%m.%d)-Lean/sha256sums
+if [ -f /tmp/upload/openwrt-rockchip*.img.gz	]; then
 	echo -e '\e[92m今天固件已下载，准备升级\e[0m'
 else
 	echo -e '\e[91m今天的固件还没更新，尝试下载昨天的固件\e[0m'
 	wget https://github.com/DHDAXCW/NanoPi-R4S-2021/releases/download/$(date -d "@$(( $(busybox date +%s) - 86400))" +%Y.%m.%d)-Lean/openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz
-	if [ -f /mnt/mmcblk0p2/openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz ]; then
+	wget https://github.com/DHDAXCW/NanoPi-R4S-2021/releases/download/$(date -d "@$(( $(busybox date +%s) - 86400))" +%Y.%m.%d)-Lean/sha256sums
+	if [ -f /tmp/upload/openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz ]; then
 		echo -e '\e[92m昨天的固件已下载，准备升级\e[0m'
 	else
 		echo -e '\e[91m没找到最新的固件，脚本退出\e[0m'
 		exit 1
 	fi
 fi
-cd /mnt/mmcblk0p2
+cd /tmp/upload
+md5sum -c md5sums 2> /dev/null | grep OK
 sysupgrade -v openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz
