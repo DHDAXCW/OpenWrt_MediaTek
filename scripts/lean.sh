1dda20911ac01feee9239ad4c4cf7a1a7eb7fa98
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-03-19 20:40:19 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -165,7 +165,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 # Custom configs
 git am $GITHUB_WORKSPACE/patches/lean/*.patch
-echo -e " Lean's OpenWrt built on "$(date +%Y.%m.%d)"\n -----------------------------------------------------" >> package/base-files/files/etc/banner
+echo -e " DHDAXCW's OpenWrt built on "$(date +%Y.%m.%d)"\n -----------------------------------------------------" >> package/base-files/files/etc/banner
 
 # Add CUPInfo
 pushd package/lean/autocore/files/arm/sbin
