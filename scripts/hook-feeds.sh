7a0c09313274cc91ab714478ac274989a0e98036
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-04-01 08:40:33 +0800

    Create hook-feeds.sh

---
+++ scripts/hook-feeds.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#=================================================
+# File name: hook-feeds.sh
+# Author: SuLingGG
+# Blog: https://mlapp.cn
+#=================================================
+
+# Svn checkout packages from immortalwrt's repository
+pushd customfeeds
+
+# Add luci-app-gowebdav
+svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
+svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
+
+# Add smartdns&luci-app-smartdns
+svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-smartdns luci/applications/luci-app-smartdns
+svn co https://github.com/immortalwrt/packages/trunk/net/smartdns packages/net/smartdns
+
+# Add tmate
+git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
+
+# Add gotop
+svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/gotop packages/admin/gotop
+popd
+
+# Add minieap
+svn co https://github.com/immortalwrt/packages/trunk/net/minieap packages/net/minieap
+
+# Set to local feeds
+pushd customfeeds/packages
+export packages_feed="$(pwd)"
+popd
+pushd customfeeds/luci
+export luci_feed="$(pwd)"
+popd
+sed -i '/src-git packages/d' feeds.conf.default
+echo "src-link packages $packages_feed" >> feeds.conf.default
+sed -i '/src-git luci/d' feeds.conf.default
+echo "src-link luci $luci_feed" >> feeds.conf.default
+
+# Update feeds
+./scripts/feeds update -a
