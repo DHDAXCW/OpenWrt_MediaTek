e61904c51b5c29e8c460aa89c506362e8f4d9ab5
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-05-06 08:20:01 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -11,7 +11,6 @@ pushd package/community
 
 # Add Lienol's Packages
 git clone --depth=1 https://github.com/Lienol/openwrt-package
-rm -rf ../lean/luci-app-kodexplorer
 
 # Add luci-app-passwall
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
