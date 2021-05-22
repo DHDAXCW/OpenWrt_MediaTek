113ca591962d74d1e8c3e02ece0de0f014a46c18
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-05-23 07:42:59 +0800

    fix netdata

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -24,10 +24,6 @@ svn co https://github.com/281677160/openwrt-package/trunk/netdata packages/admin
 rm -rf ../package/lean/luci-app-netdata
 svn co https://github.com/281677160/openwrt-package/trunk/luci-app-netdata luci/applications/luci-app-netdata
 
-# Add luci-app-smartdns
-svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-smartdns luci/applications/luci-app-smartdns
-svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/smartdns packages/net/smartdns
-
 # Add tmate
 git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
 
