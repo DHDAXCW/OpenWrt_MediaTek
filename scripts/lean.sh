f8f90ba87bfc23eec6a5a90b443cb5b22ba40e77
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-06-26 21:51:15 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -26,6 +26,9 @@ git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk
 # Add luci-proto-minieap
 git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
 
+# Add luci-app-bypass
+git clone https://github.com/garypang13/luci-app-bypass.git
+
 # Add ServerChan
 git clone --depth=1 https://github.com/tty228/luci-app-serverchan
 
