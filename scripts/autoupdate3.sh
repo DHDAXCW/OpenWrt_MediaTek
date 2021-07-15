d27da4b9a9c42fa8255304f9fdc40110451b8572
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-07-15 12:16:33 +0800

    Update autoupdate3.sh

--- scripts/autoupdate3.sh
+++ scripts/autoupdate3.sh
@@ -5,18 +5,24 @@ opkg install gzip
 cd /tmp
 rm -rf artifact openwrt-rockchip*.img.gz openwrt-rockchip*img*
 echo -e '\e[92m准备下载升级文件\e[0m'
+echo `(date +%Y.%m.%d)`
 wget https://github.com/DHDAXCW/NanoPi-R4S-2021/releases/download/$(date +%Y.%m.%d)-Lean3/openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz
 if [ -f /tmp/openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz	]; then
-	echo -e '\e[92m今天固件已下载，准备解压\e[0m'
+	echo -e '\e[92m当日固件已下载，准备解压\e[0m'
 else
-	echo -e '\e[91m今天的固件还没更新，尝试下载昨天的固件\e[0m'
-	wget https://github.com/DHDAXCW/NanoPi-R4S-2021/releases/download/$(date -d "@$(( $(busybox date +%s) - 86400))" +%Y.%m.%d)-Lean3/openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz
-	if [ -f /tmp/openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz ]; then
-		echo -e '\e[92m昨天的固件已下载，准备解压\e[0m'
-	else
-		echo -e '\e[91m没找到最新的固件，脚本退出\e[0m'
-		exit 1
-	fi
+	echo -e '\e[91m当日固件暂未更新，尝试下载旧固件\e[0m'
+	for ((t=1;t<=15;t++))
+	do {
+	    echo `(date -d "@$(( $(busybox date +%s) - 86400*$t))" +%Y.%m.%d)`
+	    wget https://github.com/DHDAXCW/NanoPi-R4S-2021/releases/download/$(date -d "@$(( $(busybox date +%s) - 86400*$t))" +%Y.%m.%d)-Lean3/openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz
+	    if [ -f /tmp/openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz ]; then
+		    echo -e '\e[92m旧版固件已下载，准备解压\e[0m'
+		    break;
+	    else
+		    echo -e '\e[91m当前固件不存在，继续寻找前一天的固件\e[0m'
+		fi
+	}
+	done
 fi
 cd /tmp
 echo -e '\e[92m准备解压镜像文件\e[0m'
