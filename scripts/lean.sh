327663a4930102f8c3655be64cab7cc5c7d22113
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-07-18 00:52:09 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -69,9 +69,9 @@ svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/pa
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8192du
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl88x2bu
 
-# Add smartdns
-svn co https://github.com/pymumu/smartdns/trunk/package/openwrt ../smartdns
-svn co https://github.com/pymumu/luci-app-smartdns/trunk ../luci-app-smartdns
+# Add luci-app-smartdns & smartdns
+svn co https://github.com/281677160/openwrt-package/trunk/feeds/luci/applications/luci-app-smartdns
+svn co https://github.com/281677160/openwrt-package/trunk/feeds/packages/net/smartdns
 
 # 动态DNS
 git clone --depth 1 https://github.com/small-5/ddns-scripts-dnspod package/lean/ddns-scripts_dnspod
