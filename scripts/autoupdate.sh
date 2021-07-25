e05bb1fe98c8ffd5a695233ccc16450ae040ca24
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-07-25 23:03:53 +0800

    Update autoupdate.sh

--- scripts/autoupdate.sh
+++ scripts/autoupdate.sh
@@ -1,4 +1,6 @@
 #!/bin/sh --Created by DHDAXCW
+wget 'https://git.openwrt.org/?p=keyring.git;a=blob_plain;f=usign/1035ac73cc4e59e3' -O 1035ac73cc4e59e3 && 
+opkg-key add 1035ac73cc4e59e3
 opkg update
 # opkg install pv
 opkg install gzip 
