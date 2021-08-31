01bd306738771bcb2554cc2c39afd0237f70ab38
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-08-31 13:27:09 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -8,6 +8,12 @@
 # Svn checkout packages from immortalwrt's repository
 pushd customfeeds
 
+# Add luci-app-eqos
+svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos luci/applications/luci-app-eqos
+
+# Add luci-proto-modemmanager
+svn co https://github.com/immortalwrt/luci/trunk/protocols/luci-proto-modemmanager luci/protocols/luci-proto-modemmanager
+
 # Add luci-app-gowebdav
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
 svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
