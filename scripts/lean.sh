a552f1384e1ebf16f5a6e542821fbd3febed557b
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-10-18 08:54:39 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -172,7 +172,7 @@ sed -i "s/OpenWrt /DHDAXCW @ FusionWrt $(TZ=UTC-8 date "+%Y%m%d") /g" package/le
 
 # Custom configs
 # git am $GITHUB_WORKSPACE/patches/lean/*.patch
-# git am $GITHUB_WORKSPACE/patches/*.patch
+git am $GITHUB_WORKSPACE/patches/*.patch
 echo -e " DHDAXCW's FusionWrt built on "$(date +%Y.%m.%d)"\n -----------------------------------------------------" >> package/base-files/files/etc/banner
 echo 'net.bridge.bridge-nf-call-iptables=0' >> package/base-files/files/etc/sysctl.conf
 echo 'net.bridge.bridge-nf-call-ip6tables=0' >> package/base-files/files/etc/sysctl.conf
