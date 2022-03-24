ecc17c929bfa0ae42cbe8d65a3ebd650126f957c
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-03-24 20:54:39 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -197,7 +197,7 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='FusionWrt'' pac
 sed -i "s/OpenWrt /DHDAXCW @ FusionWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # Test kernel 5.10
-sed -i 's/5.15/5.10/g' target/linux/rockchip/Makefile
+sed -i 's/5.15/5.4/g' target/linux/rockchip/Makefile
 
 # upgrade the kernel
 #pushd include
