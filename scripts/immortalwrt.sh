d8a0d51c351d3367aa4afed3e2a6ceb0ae4dfb98
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-04-03 20:20:40 +0800

    Create immortalwrt.sh

---
+++ scripts/immortalwrt.sh
@@ -0,0 +1,33 @@
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Mod zzz-default-settings
+pushd package/emortal/default-settings/files
+sed -i '/http/d' zzz-default-settings
+sed -i '/openwrt_luci/d' zzz-default-settings
+popd
+
+# Add date version
+export DATE_VERSION=$(date -d "$(rdate -n -4 -p pool.ntp.org)" +'%Y-%m-%d')
+sed -i "s/%C/%C (${DATE_VERSION})/g" package/base-files/files/etc/openwrt_release
+
+# Clone community packages to package/community
+mkdir package/community
+pushd package/community
+
+# Add luci-app-oaf
+git clone --depth=1 https://github.com/destan19/OpenAppFilter -b oaf-3.0.1
+popd
+
+# Fix mt76 wireless driver
+pushd package/kernel/mt76
+sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
+popd
+
+# Rename hostname to OpenWrt
+pushd package/base-files/files/bin
+sed -i 's/ImmortalWrt/OpenWrt/g' config_generate
+popd
+
+# Change default shell to zsh
+sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
