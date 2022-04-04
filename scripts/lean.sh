4f009a286fc1378aff86b22a0b34d9a171a378d7
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-04-04 19:21:03 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -16,10 +16,8 @@ sed -i 's,2016,2208,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-def
 sed -i 's,1512,1608,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/cpufreq
 
 # 牛逼
-rm -rf ./package/boot
-svn co https://github.com/LPDDR6-10000MHz/immortalwrt-DoorNET2/branches/openwrt-18.06-k5.4/package/boot ./package/boot
-rm -rf ./target/linux/rockchip
-svn co https://github.com/LPDDR6-10000MHz/immortalwrt-DoorNET2/branches/openwrt-18.06-k5.4/target/linux/rockchip ./target/linux/rockchip
+rm -rf ./target/linux/rockchip/patches-5.4
+svn co https://github.com/LPDDR6-10000MHz/immortalwrt-DoorNET2/branches/openwrt-18.06-k5.4/target/linux/rockchip/patches-5.4 ./target/linux/rockchip/patches-5.4
 
 # Clone community packages to package/community
 mkdir package/community
