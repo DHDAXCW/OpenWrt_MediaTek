2bdc9f6862d6c6d4fbf826af6fc50abbcd625d16
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-04-10 14:55:11 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -7,7 +7,7 @@ svn co https://github.com/DHDAXCW/packages/branches/ok/admin/netdata ./feeds/pac
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall ./package/lean/openwrt-passwall
 svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall ./package/lean/openwrt-passwall
-
+rm -rf ./package/feeds/packages/baidupcs-web
 pushd target/linux/rockchip/image
 rm -rf armv8.mk
 wget https://raw.githubusercontent.com/DHDAXCW/lede/master/target/linux/rockchip/image/armv8.mk
