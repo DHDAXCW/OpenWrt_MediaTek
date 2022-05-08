68a46cb0f429ac85805fa6f5b093b09364579e31
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-05-08 19:24:31 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -147,7 +147,7 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='FusionWrt'' pac
 sed -i "s/OpenWrt /DHDAXCW @ FusionWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 pushd target/linux/rockchip/patches-5.15
-cp -f $GITHUB_WORKSPACE/scripts/patchs/994-wode.patch 994-wode.patch
+cp -f $GITHUB_WORKSPACE/scripts/patch/994-wode.patch 994-wode.patch
 popd
 
 # Custom configs
