9894ad1b5975931c7d79d62dbd1b65372e28c8c2
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-05-21 18:47:11 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -135,6 +135,9 @@ sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_genera
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='FusionWrt'' package/lean/default-settings/files/zzz-default-settings
 sed -i "s/OpenWrt /DHDAXCW @ FusionWrt /g" package/lean/default-settings/files/zzz-default-settings
 
+rm -rf package/libs/wolfssl
+svn co https://github.com/Boos4721/openwrt/trunk/package/libs/wolfssl package/libs/wolfssl
+
 # 删除定时coremark
 rm -rf ./customfeeds/packages/utils/coremark
 svn co https://github.com/DHDAXCW/packages/trunk/utils/coremark customfeeds/packages/utils/coremark
