6d6d9c7ab9881680b88fb27e1782ce3c9215ca98
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-05-27 10:02:56 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -131,13 +131,11 @@ popd
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 # sed -i 's/5.15/5.10/g' target/linux/rockchip/Makefile
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='FusionWrt'' package/lean/default-settings/files/zzz-default-settings
 sed -i "s/OpenWrt /DHDAXCW @ FusionWrt /g" package/lean/default-settings/files/zzz-default-settings
-
-rm -rf package/libs/wolfssl
-svn co https://github.com/Boos4721/openwrt/trunk/package/libs/wolfssl package/libs/wolfssl
 
 # 删除定时coremark
 rm -rf ./customfeeds/packages/utils/coremark
