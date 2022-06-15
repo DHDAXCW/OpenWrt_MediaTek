0bff0a39f97466363ef310b4e6678f8a36bdf9e0
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-06-15 23:45:35 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -142,7 +142,6 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
-
 # 删除定时coremark
 rm -rf ./customfeeds/packages/utils/coremark
 svn co https://github.com/DHDAXCW/packages/trunk/utils/coremark customfeeds/packages/utils/coremark
