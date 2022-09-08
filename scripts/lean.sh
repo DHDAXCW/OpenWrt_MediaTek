b0ca295d7a8525f5a29f305d1d7deef3bb6d6227
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-09-08 11:31:01 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -60,13 +60,11 @@ git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 
 # Add luci-app-adguardhome
-svn co https://github.com/Boos4721/OpenWrt-Packages/trunk/luci-app-adguardhome
+svn co https://github.com/Lienol/openwrt-package/branches/other/luci-app-adguardhome
 
 # Add ddnsto & linkease
 svn co https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-ddnsto
-svn co https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-linkease
 svn co https://github.com/linkease/nas-packages/trunk/network/services/ddnsto
-svn co https://github.com/linkease/nas-packages/trunk/network/services/linkease
 
 # Add luci-app-onliner (need luci-app-nlbwmon)
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
