79dc0b463f0abed5700fee978e13819f233816a0
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-10-20 14:51:51 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -54,7 +54,8 @@ git clone --depth=1 https://github.com/UnblockNeteaseMusic/luci-app-unblocknetea
 
 # Add luci-app-vssr <M>
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
-git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
+# git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
+git clone -b ok https://github.com/DHDAXCW/luci-app-vssr
 
 # Add luci-proto-minieap
 git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
