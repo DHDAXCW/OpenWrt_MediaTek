5bbf29be360900e153772bbd180a4edc192a68b3
zonyitoo <zonyitoo@gmail.com>
2022-10-25 14:24:07 +0800

    luci-app-vssr mods merged to upstream

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -54,7 +54,7 @@ git clone --depth=1 https://github.com/UnblockNeteaseMusic/luci-app-unblocknetea
 
 # Add luci-app-vssr <M>
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/zonyitoo/luci-app-vssr
+git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr 
 
 # Add luci-proto-minieap
 git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
