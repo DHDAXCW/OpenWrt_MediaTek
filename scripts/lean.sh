944c8a03a2db63147161cbf667e743636fe6c7bb
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-10-27 20:09:31 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -15,13 +15,6 @@ sed -i 's,1608,1800,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-def
 sed -i 's,2016,2208,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/10-cpufreq
 sed -i 's,1512,1608,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/10-cpufreq
 # 🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧
-#rm -rf ./target/linux/rockchip/armv8/base-files/etc/hotplug.d/usb
-#rm -rf package/kernel/mac80211
-#svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/mac80211 package/kernel/mac80211
-rm -rf package/kernel/rtl8821cu
-#svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8821cu package/kernel/rtl8821cu
-#rm -rf package/kernel/mwlwifi
-#svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/mwlwifi package/kernel/mwlwifi
 
 # alist
 git clone https://github.com/sbwml/luci-app-alist package/alist
