6ac28b87e8777b31095d8eebc5c2fa15e7de20ad
骷髅头 <lasstp5011@gmail.com>
2023-07-07 14:18:03 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -12,7 +12,7 @@
 # alist
 git clone https://github.com/sbwml/luci-app-alist package/alist
 rm -rf feeds/packages/lang/golang
-svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 
 # Clone community packages
 mkdir package/community
