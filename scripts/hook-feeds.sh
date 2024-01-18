68582056ffe39b3c5162773d404519ca81084f01
骷髅头 <lasstp5011@gmail.com>
2024-01-18 09:31:16 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -7,32 +7,6 @@
 # Author: SuLingGG
 # Blog: https://mlapp.cn
 #=================================================
-# Svn checkout packages from immortalwrt's repository
-pushd customfeeds
-
-# Add luci-app-eqos
-svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos luci/applications/luci-app-eqos
-
-# Add luci-proto-modemmanager
-svn co https://github.com/immortalwrt/luci/trunk/protocols/luci-proto-modemmanager luci/protocols/luci-proto-modemmanager
-
-# Add luci-app-gowebdav
-svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
-svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
-
-# Add tmate
-git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
-
-# Add gotop
-svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/gotop packages/admin/gotop
-
-# Add minieap
-svn co https://github.com/immortalwrt/packages/trunk/net/minieap packages/net/minieap
-
-# Replace smartdns with the official version
-#rm -rf packages/net/smartdns
-#svn co https://github.com/openwrt/packages/trunk/net/smartdns packages/net/smartdns
-popd
 
 # Set to local feeds
 pushd customfeeds/packages
